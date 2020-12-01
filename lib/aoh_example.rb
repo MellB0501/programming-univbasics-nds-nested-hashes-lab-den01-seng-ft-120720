647c8ea85e14d2d88d916179a2d7f12c69f310a6
Mell Basham <mellbasham@gmail.com>
2020-12-01 01:44:27 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -20,7 +20,11 @@ end
 
 def literal_aoh
   literal_aoh = [
-    ]
+    {},
+    {},
+    {},
+    {},
+    {}]
   # Using Array literal syntax only, build a nested array that uses the data in
   # held in the constants
 end
